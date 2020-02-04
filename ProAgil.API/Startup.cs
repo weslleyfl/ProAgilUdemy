33e6736fddbbbc07b7bf086c9b1ab57832117c57
weslley lopes <weslleyfl@yahoo.com.br>
2020-02-04 21:30:32 -0200

    Adicionei bootstrap e fontawesome no angular

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -34,6 +34,7 @@ namespace ProAgil.API {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
             } else {
